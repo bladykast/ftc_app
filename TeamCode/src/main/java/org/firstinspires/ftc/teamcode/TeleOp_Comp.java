c4f7a163345d5aa344f56edebe3cb74b88b56e0c
Thomas Bladykas <bladykas.thomas@gmail.com>
2017-12-03 22:18:44 -0500

    Merging Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Comp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Comp.java
@@ -3,9 +3,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptRampMotorSpeed;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "3736: TeleOp", group = "TeleOp")
 
@@ -14,25 +18,35 @@ public class TeleOp_Comp extends OpMode {
 
 	final static double JEWEL_MIN_RANGE  = 0;
 	final static double JEWEL_MAX_RANGE  = 1;
-	final static double GLYPH_MIN_RANGE  = 0;
-	final static double GLYPH_MAX_RANGE  = 1;
-
-
+	final static double GLYPHDUMP_MIN_RANGE  = 0;
+	final static double GLYPHDUMP_MAX_RANGE  = 1;
 
 	double jewelPosition;
-	double glyphPosition;
+	double glyphdumpPosition;
+	double clawPosition;
 
-	double jewelDelta = 0.1;
-	double glyphDelta = 0.1;
+	double jewelUp = 1;
+	double jewelDown = 0.5;
+	double glyphdumpUp = 0.1;
+    double glyphdumpDown = 1;
+    double clawIn;
+    double clawOut;
 
-	DcMotor lift;
 	DcMotor strafe;
 	DcMotor rightSideFront;
 	DcMotor rightSideBack;
 	DcMotor leftSideFront;
 	DcMotor leftSideBack;
-	Servo jewel;
-	Servo glyph;
+    DcMotor glyphlift;
+	//DcMotor glyph1;
+	//DcMotor glyph2;
+
+	//CRServo relicIO;
+	//CRServo relicUD;
+
+	Servo glyphdump;
+    Servo jewel;
+    //Servo claw;
 
 
 	public TeleOp_Comp() {
@@ -47,20 +61,29 @@ public class TeleOp_Comp extends OpMode {
 		rightSideBack = hardwareMap.dcMotor.get("MRB");
 		leftSideFront = hardwareMap.dcMotor.get("MLF");
 		leftSideBack = hardwareMap.dcMotor.get("MLB");
-		lift = hardwareMap.dcMotor.get("LFT");
-        //thing
+		glyphlift = hardwareMap.dcMotor.get("LFT");
+        //glyph1 = hardwareMap.dcMotor.get("GLY1");
+        //glyph2 = hardwareMap.dcMotor.get("GLY2");
 
-		lift.setDirection(DcMotor.Direction.FORWARD);
+        jewel = hardwareMap.servo.get("JWL");
+        glyphdump = hardwareMap.servo.get("GLY");
+        //claw = hardwareMap.servo.get("CLAW");
+
+        //relicIO = hardwareMap.crservo.get("RIO");
+        //relicUD = hardwareMap.crservo.get("RUD");
+
 		rightSideFront.setDirection(DcMotor.Direction.FORWARD);
 		rightSideBack.setDirection(DcMotor.Direction.REVERSE);
 		leftSideFront.setDirection(DcMotor.Direction.REVERSE);
 		leftSideBack.setDirection(DcMotor.Direction.FORWARD);
 		strafe.setDirection(DcMotor.Direction.FORWARD);
-		jewel = hardwareMap.servo.get("JWL");
-		glyph = hardwareMap.servo.get("GLY");
+        glyphlift.setDirection(DcMotor.Direction.FORWARD);
+        //glyph1.setDirection(DcMotor.Direction.FORWARD);
+        //glyph2.setDirection(DcMotor.Direction.FORWARD);
 
 		jewelPosition = 1;
-		glyphPosition = 0.1;
+		glyphdumpPosition = 0.1;
+		clawPosition = 0.1;
 
 	}
 
@@ -70,6 +93,8 @@ public class TeleOp_Comp extends OpMode {
 		float y2 = gamepad1.right_stick_y;
 		float y3 = gamepad2.left_stick_y;
 		float y4 = gamepad2.right_stick_y;
+		boolean b1 = gamepad2.left_bumper;
+		boolean b2 = gamepad2.right_bumper;
 		boolean dleft = gamepad1.dpad_left;
 		boolean dright = gamepad1.dpad_right;
 
@@ -84,11 +109,13 @@ public class TeleOp_Comp extends OpMode {
 		y4 = (float)scaleInput(y4);
 
 
+		//Drivetrain Code
+
 		leftSideFront.setPower(y1);
 		leftSideBack.setPower(y1);
 		rightSideFront.setPower(y2);
 		rightSideBack.setPower(y2);
-		lift.setPower(y3);
+		glyphlift.setPower(y3);
 
 		if (gamepad1.dpad_left)
 		{
@@ -101,37 +128,95 @@ public class TeleOp_Comp extends OpMode {
 		else strafe.setPower(0);
 
 
-		if (gamepad2.b)
-	    {
-		    jewelPosition += jewelDelta;
-	    }
+        //Jewel Code
 
-		if (gamepad2.y)
-		{
-			jewelPosition -= jewelDelta;
-		}
+		//if (gamepad2.b)
+	    //{
+		//    jewelPosition = jewelUp;
+	    //}
+
+		//if (gamepad2.y)
+		//{
+		//	jewelPosition = jewelDown;
+		//}
+
+        //Glyph Code
 
 		if (gamepad2.x)
 		{
-			glyphPosition += glyphDelta;
+			glyphdumpPosition = glyphdumpUp;
 		}
-
 		if (gamepad2.a)
 		{
-			glyphPosition -= glyphDelta;
+			glyphdumpPosition = glyphdumpDown;
 		}
 
+        //if (gamepad1.left_bumper)
+        //{
+        //    glyph1.setPower(-1);
+        //    glyph2.setPower(-1);
+        //}
+        //else if (gamepad1.right_bumper)
+        //{
+        //    glyph1.setPower(1);
+        //    glyph2.setPower(1);
+        //}
+        //else
+        //{
+        //    glyph1.setPower(0);
+        //   glyph2.setPower(0);
+        //}
 
 
-		jewelPosition = Range.clip(jewelPosition, JEWEL_MIN_RANGE, JEWEL_MAX_RANGE);
-        glyphPosition = Range.clip(glyphPosition, GLYPH_MIN_RANGE, GLYPH_MAX_RANGE);
+		//Relic Code
+
+        //relicUD.setPower(y4);
+
+		//if (gamepad2.dpad_left)
+        //{
+        //    relicIO.setPower(1);
+        //}
+        //else if (gamepad2.dpad_right)
+        //{
+        //    relicIO.setPower(-1);
+        //}
+        //else relicIO.setPower(0);
+
+        //if (gamepad2.b)
+        //{
+        //    clawPosition = clawIn;
+        //}
+        //if (gamepad2.y)
+        //{
+        //    clawPosition = clawOut;
+        //}
+
+
+
+        //Super Mario linear code (hold more to go further...)
+
+        //if (gamepad2.x)
+        //{
+        //    glyphdumpPosition =+ glyphDelta;
+        //}
+
+        //if (gamepad2.a)
+        //{
+        //    glyphdumpPosition =- glyphDelta;
+        //}
+
+		//jewelPosition = Range.clip(jewelPosition, JEWEL_MIN_RANGE, JEWEL_MAX_RANGE);
+        //glyphdumpPosition = Range.clip(glyphdumpPosition, GLYPHDUMP_MIN_RANGE, GLYPHDUMP_MAX_RANGE);
+
 
 		jewel.setPosition(jewelPosition);
-        glyph.setPosition(glyphPosition);
+        //claw.setPosition(clawPosition);
+        glyphdump.setPosition(glyphdumpPosition);
 
 		telemetry.addData("Text", "*** Robot Data***");
 		telemetry.addData("jewel", "jewel:  " + String.format("%.2f", jewelPosition));
-        telemetry.addData("glyph", "glyph:  " + String.format("%.2f", glyphPosition));
+        //telemetry.addData("claw", "claw:  " + String.format("%.2f", clawPosition));
+		telemetry.addData("glyph", "glyph:  " + String.format("%.2f", glyphdumpPosition));
 	}
 
 
